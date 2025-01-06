50cb5bf3b6e8d6aa30c6435283e9a9745ece4f2c
Vishwajeet Londhe <vishwajeetlondhe51@gmail.com>
2025-01-06 20:33:23 +0530

    get post put method

--- app.js
+++ app.js
@@ -2,7 +2,15 @@ const express = require('express');
 const app = express();
 
 app.get('/checkAPI', function(req, res){
-    res.send('Hello API!');
+    res.send('Hello API get method');
+})
+
+app.post('/checkAPI', function(req, res){
+    res.send('Hello API post method');
+})
+
+app.put('/checkAPI', function(req, res){
+    res.send('Hello API put method');
 })
 
 app.listen(3000);
