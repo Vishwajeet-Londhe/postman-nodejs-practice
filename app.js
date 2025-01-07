a2ca0503e0314206c7ded96e3a5918b88638ee60
Vishwajeet Londhe <vishwajeetlondhe51@gmail.com>
2025-01-07 21:33:47 +0530

    app crashed top level error

--- app.js
+++ app.js
@@ -1,28 +1,45 @@
-const express = require('express');
+// postman options check
+
+// const express = require('express');
+// const app = express();
+
+// app.get('/checkAPI', function(req, res){
+//     res.send('Hello API get method');
+// })
+
+// app.post('/checkAPI', function(req, res){
+//     res.send('Hello API post method');
+// })
+
+// app.put('/checkAPI', function(req, res){
+//     res.send('Hello API put method');
+// })
+
+// app.patch('/checkAPI', function(req, res){
+//     res.send('Hello API patch method');
+// })
+
+// app.delete('/checkAPI', function(req, res){
+//     res.send('Hello API delete method');
+// })
+
+// app.head('/checkAPI', function(req, res){
+//     res.send('Hello API head method');
+// })
+
+// app.listen(3000);
+
+
+
+
+
+// error handling
+
+const express = require("express");
 const app = express();
 
-app.get('/checkAPI', function(req, res){
-    res.send('Hello API get method');
-})
-
-app.post('/checkAPI', function(req, res){
-    res.send('Hello API post method');
-})
-
-app.put('/checkAPI', function(req, res){
-    res.send('Hello API put method');
-})
-
-app.patch('/checkAPI', function(req, res){
-    res.send('Hello API patch method');
-})
-
-app.delete('/checkAPI', function(req, res){
-    res.send('Hello API delete method');
-})
-
-app.head('/checkAPI', function(req, res){
-    res.send('Hello API head method');
+app.fat("/", function(req, res){
+    res.send("he ram");
 })
 
 app.listen(3000);
