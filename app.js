f5bbe98b53df633f0474da6b599db3e3cf840423
Vishwajeet Londhe <vishwajeetlondhe51@gmail.com>
2025-01-07 22:08:10 +0530

    next error solve

--- app.js
+++ app.js
@@ -38,7 +38,7 @@
 const express = require("express");
 const app = express();
 
-app.get("/", function(req, res){
+app.get("/", function(req, res, next){
     try{
         res.send(hey);    
     }
